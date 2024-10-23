8923fa461c70137f57a609f13edf1e79359b522a
keya1331 <keya.patel371@gmail.com>
2024-10-23 13:00:34 +0530

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='flex items-center justify-between gap-4 p-4'>
+            <a href="/"><h1 className='font-medium'>Echo<span className='text-blue-400 bold'>Quill</span></h1></a>
+            <div className='gap-4 flex items-center '>
+                
+            </div>
+        </header>
+    )
+}
